de68d5d352bcb7e55e4ac8fb484586e256d0272f
HaithamSheshtawy <h.sheshtawy@tap.company>
2020-04-02 12:19:12 +0300

    integrate with flurry

--- app/src/main/java/company/tap/tapcardscannerkit_android/MyApplication.java
+++ app/src/main/java/company/tap/tapcardscannerkit_android/MyApplication.java
@@ -1,4 +1,22 @@
 package company.tap.tapcardscannerkit_android;
 
-public class MyApplication {
+import android.app.Application;
+import android.util.Log;
+
+import com.flurry.android.FlurryAgent;
+import com.flurry.android.FlurryPerformance;
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        new FlurryAgent.Builder()
+                .withDataSaleOptOut(false) //CCPA - the default value is false
+                .withCaptureUncaughtExceptions(true)
+                .withIncludeBackgroundSessionsInMetrics(true)
+                .withLogLevel(Log.VERBOSE)
+                .withPerformanceMetrics(FlurryPerformance.ALL)
+                .build(this, "87SBKCH7W7MD6S7P4DNQ");
+    }
 }
