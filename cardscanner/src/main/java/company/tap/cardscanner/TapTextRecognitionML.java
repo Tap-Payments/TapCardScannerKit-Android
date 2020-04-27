02e764d29318da74e46efc6178f99d90830a9047
AhlaamK-tap <a.kazi@tap.company>
2020-04-27 15:35:09 +0300

    fixes java 1.8

--- cardscanner/src/main/java/company/tap/cardscanner/TapTextRecognitionML.java
+++ cardscanner/src/main/java/company/tap/cardscanner/TapTextRecognitionML.java
@@ -2,6 +2,7 @@ package company.tap.cardscanner;
 
 import android.graphics.Bitmap;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.ml.vision.FirebaseVision;
 import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 import com.google.firebase.ml.vision.text.FirebaseVisionText;
