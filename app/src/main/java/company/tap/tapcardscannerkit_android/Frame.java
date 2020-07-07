1c4cd1111511301ac4809776a3500cd90322f983
AhlaamK-tap <a.kazi@tap.company>
2020-07-07 14:50:48 +0300

    commit

--- app/src/main/java/company/tap/tapcardscannerkit_android/Frame.java
+++ app/src/main/java/company/tap/tapcardscannerkit_android/Frame.java
@@ -1,6 +1,11 @@
 package company.tap.tapcardscannerkit_android;
 
-import View;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
 
 /**
  * Created by AhlaamK on 5/27/20.
@@ -9,4 +14,17 @@ import View;
  * All rights reserved.
  **/
 public class Frame extends View {
+    public Frame(Context context) {
+        super(context);
+    }
+
+    public Frame(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Frame(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
 }
