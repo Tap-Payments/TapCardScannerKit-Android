a0a03439ef64d140a164b0265bf544e8d1909d99
AhlaamK-tap <57221514+AhlaamK-tap@users.noreply.github.com>
2025-01-27 13:38:46 +0300

    NPE fix

--- cardscanner/src/main/java/company/tap/cardscanner/CameraFragment.java
+++ cardscanner/src/main/java/company/tap/cardscanner/CameraFragment.java
@@ -203,6 +203,7 @@ public class CameraFragment extends Fragment implements SurfaceHolder.Callback ,
                // Bitmap bitmap = Bitmap.createBitmap(bmp, 0, rectCrop.top, rectCrop.width(), rectCrop.height() );
 
               //  System.out.println("bitmap frag is"+bitmap);
+               if(_context!=null) FirebaseApp.initializeApp(_context);
                 //initializing FirebaseVisionTextRecognizer object
                 FirebaseVisionTextRecognizer detector = FirebaseVision.getInstance().getOnDeviceTextRecognizer();
                 //Passing FirebaseVisionImage Object created from the cropped bitmap
